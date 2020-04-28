05e0c1f571ca947e1d34100d831a2cdff7370f2e
Kanigolla Sai Ganesh <ganesh.kanigolla@imaginea.com>
2020-04-28 21:29:03 +0530

    added full animations through greensock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,9 @@
 import { Component } from '@angular/core';
-import * as Highcharts from 'highcharts';
-import { LINECHART } from './models/line-chart.model';
-import { BARCHART } from './models/bar-chart.model';
-import { BUBBLECHART } from './models/bubble-chart.model';
-import { PIECHART } from './models/pie-chart.model';
-import { STACKCHART } from './models/stack-chart.model';
-import { SPLINECHART } from './models/spline-chart.model';
-import { COMBINATIONCHART } from './models/combination-chart.model';
-
-import { WORLDMAPMODEL } from './models/world-map.model';
-import { POPULATIONMAPMODEL } from './models/population-map.model';
-import { TEMPERATUREMAPMODEL } from './models/world-temperature.model';
-
-
-import * as Highchartmaps from 'highcharts/highmaps';
-
-
-
-
-declare var require: any;
-require('highcharts/modules/exporting')(Highcharts);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'highChartsPoc';
-  Highcharts = Highcharts;
 
-  Highchartmaps = Highchartmaps;
-  charts = [
-    'Line Chart', 'Bar Chart', 'Bubble Chart',
-    'Pie Chart', 'Stack Chart', 'Spline Chart',
-    'Combination Chart', 'slider', 'World Map', 'Population Map', 'Temperature Map'];
-  selectedItem = this.charts[0];
-
-
-  chartConstructor = 'mapChart';
-
-  lineChartOptions = LINECHART;
-  barChartOptions = BARCHART;
-  bubbleChartOptions = BUBBLECHART;
-  pieChartOptions = PIECHART;
-  stackChartOptions = STACKCHART;
-  splineChartOptions = SPLINECHART;
-  combinationChartOptions = COMBINATIONCHART;
-  worldMapOptions = WORLDMAPMODEL;
-  populationMapOptions = POPULATIONMAPMODEL;
-  temperatureMapOptions = TEMPERATUREMAPMODEL;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,16 @@ import { HighchartsChartModule } from 'highcharts-angular';
 import { SliderComponent } from './shared/components/slider/slider.component';
 import { Ng5SliderModule } from 'ng5-slider';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HighchartsComponent } from './pages/highcharts/highcharts.component';
+import { GreensockAnimationComponent } from './pages/greensock-animation/greensock-animation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SliderComponent
+    SliderComponent,
+    HighchartsComponent,
+    GreensockAnimationComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +24,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HighchartsChartModule,
     Ng5SliderModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule
 
   ],
   providers: [],
