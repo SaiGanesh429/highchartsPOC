55c459b9cb9704e43ae4c8082adb54f2c30b5d17
Kanigolla Sai Ganesh <ganesh.kanigolla@imaginea.com>
2020-05-07 12:27:52 +0530

    added background image

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'highChartsPoc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('highChartsPoc');
+    // expect(app.title).toEqual('highChartsPoc');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // this.checkTime();
+    setInterval(this.checkTime, 5000);
+  }
+
+  checkTime() {
+    const n = new Date().getHours();
+    if (n > 15 || n < 6) {
+      document.getElementsByClassName("main")[0].classList.remove("day");
+      document.getElementsByClassName("main")[0].classList.add("night");
+    }
+    else {
+      document.getElementsByClassName("main")[0].classList.remove("night");
+      document.getElementsByClassName("main")[0].classList.add("day");
+    }
+  }
 }
