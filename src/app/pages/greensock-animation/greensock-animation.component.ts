c8a2707f5a9e8a350bbfaf31bc7759261d40e05e
Kanigolla Sai Ganesh <ganesh.kanigolla@imaginea.com>
2020-05-13 12:22:39 +0530

    added live news with static data

--- src/app/pages/greensock-animation/greensock-animation.component.ts
+++ src/app/pages/greensock-animation/greensock-animation.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import gsap from 'gsap';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+import { TimelineMax, Linear } from 'gsap';
+declare var $: any;
+
 import { Router } from '@angular/router';
 gsap.registerPlugin(ScrollToPlugin);
 @Component({
@@ -11,8 +14,9 @@ gsap.registerPlugin(ScrollToPlugin);
 export class GreensockAnimationComponent implements OnInit {
 
   items = [
-    'move', 'bounce', 'rotate', 'scroll', 'scrollToPlugin'];
+    'move', 'bounce', 'rotate', 'scroll', 'scrollToPlugin', 'news Scroll'];
   selectedItem = this.items[0];
+  infinite;
   constructor(private router: Router) { }
   ngOnInit(): void {
   }
@@ -32,7 +36,59 @@ export class GreensockAnimationComponent implements OnInit {
       case 'scroll':
         this.scroll();
         break;
+      case 'news Scroll':
+        this.liveNews();
+        break;
     }
+  }
+
+
+
+  liveNews() {
+
+    $(document).ready(() => {
+      const $tickerWrapper = $('.tickerwrapper');
+      const $list = $tickerWrapper.find('ul.list');
+      const $clonedList = $list.clone();
+      let listWidth = 10;
+
+      $list.find('li').each(function (i) {
+        listWidth += $(this, i).outerWidth(true);
+      });
+
+      // const endPos = $tickerWrapper.width() - listWidth;
+
+      $list.add($clonedList).css({
+        width: listWidth + 'px'
+      });
+
+      $clonedList.addClass('cloned').appendTo($tickerWrapper);
+
+      //TimelineMax
+      const infinite = new TimelineMax({ repeat: -1, paused: true });
+      const time = 10;
+
+      infinite
+        .fromTo($list, time, { rotation: 0.01, x: 0 }, { force3D: true, x: -listWidth, ease: Linear.easeNone }, 0)
+        .fromTo($clonedList, time, { rotation: 0.01, x: listWidth }, { force3D: true, x: 0, ease: Linear.easeNone }, 0)
+        .set($list, { force3D: true, rotation: 0.01, x: listWidth })
+        .to($clonedList, time, { force3D: true, rotation: 0.01, x: -listWidth, ease: Linear.easeNone }, time)
+        .to($list, time, { force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone }, time)
+        .progress(1).progress(0)
+        .play();
+
+      $tickerWrapper.on('mouseenter', () => {
+        infinite.pause();
+      }).on('mouseleave', () => {
+        infinite.play();
+      });
+
+    });
+  }
+
+
+  NavigateToCharts() {
+    this.router.navigate(['/charts']);
   }
 
   scrollToPlugin() {
