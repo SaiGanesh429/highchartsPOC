b9d20ea120068837c5aac67663b649ad8140a998
Kanigolla Sai Ganesh <ganesh.kanigolla@imaginea.com>
2020-06-12 20:15:35 +0530

    added table , increment ,decrement buttons , opacity effect for table and line -dotted chart combination

---
+++ src/app/pages/table/table.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+  enableData = false;
+  num = 30
+  ngOnInit(): void {
+  }
+  setFlag(value) {
+    this.enableData = value;
+  }
+
+  updateValue(value) {
+    value === 'plus' ? this.num++ : this.num--;
+  }
+}
